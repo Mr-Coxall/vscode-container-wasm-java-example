03145e7c54f99de8f5d0e09a0038b8271840681f
Patrick Coxall <mr.coxall@mths.ca>
2024-06-10 11:13:02 -0400

    Initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sept 2020
+// This is the "Hello, World!" program, with proper style
+
+#include <stdio.h>
+
+int main(void) {
+    // This function just prints Hello, World!
+    printf("Hello, World!\n");
+
+    printf("\nDone.\n");
+    return 0;
+}
